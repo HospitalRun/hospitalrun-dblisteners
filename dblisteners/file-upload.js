93543511c581e38ce1dd1fef58a0fba4d2fe5790
John Kleinschmidt <kleinschmidtorama@gmail.com>
2017-02-16 16:03:41 -0500

    Fix uploading attachments

--- dblisteners/file-upload.js
+++ dblisteners/file-upload.js
@@ -3,7 +3,7 @@ var mkdirp = require('mkdirp');
 var path = require('path');
 
 function getFilePaths(type, fileName, config) {
-  switch(type) {
+  switch (type) {
     case 'photo': {
       return {
         fileName: '/patientimages' + fileName,
@@ -22,7 +22,7 @@ function getFilePaths(type, fileName, config) {
  * Upload file that is temporarily stored as attachment.
  */
 module.exports = function(change, maindb, config) {
-  if (change.doc && change.doc.data.localFile && change.doc.data.localFile === true && change.doc._attachments) {
+  if (change.doc && change.doc.data && change.doc.data.localFile && change.doc.data.localFile === true && change.doc._attachments) {
     try {
 
       var currentDoc = change.doc;
@@ -48,14 +48,15 @@ module.exports = function(change, maindb, config) {
               return;
             }
             // Remove the attachment from the document
-            maindb.attachment.destroy(currentDoc._id, 'file', {rev: currentDoc._rev }, function(err, body) {
+            maindb.attachment.destroy(currentDoc._id, 'file', {rev: currentDoc._rev}, function(err, body) {
               if (err) {
                 console.log('Error deleting attachment on ' + currentDoc._id + ', rev:' + currentDoc._rev, err);
                 return;
               }
               currentDoc._rev = body.rev;
-              currentDoc.data.url = filePaths;
+              currentDoc.data.url = filePaths.fileName;
               currentDoc.data.localFile = false;
+              currentDoc.data.files = [];
               delete currentDoc._attachments;
               // Update the url
               maindb.insert(currentDoc, currentDoc._id, function(err) {
