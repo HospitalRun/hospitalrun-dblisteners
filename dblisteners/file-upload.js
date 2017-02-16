452aad2bf51dd849627f69f602d8ec484285d3ed
John Kleinschmidt <kleinschmidtorama@gmail.com>
2017-02-16 10:06:52 -0500

    Fixed file upload to work with ember-pouch data structure

--- dblisteners/file-upload.js
+++ dblisteners/file-upload.js
@@ -1,19 +1,38 @@
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var path = require('path');
+
+function getFilePaths(type, fileName, config) {
+  switch(type) {
+    case 'photo': {
+      return {
+        fileName: '/patientimages' + fileName,
+        filePath: config.imagesdir + fileName
+      };
+    }
+    case 'attachment': {
+      return {
+        fileName: '/attachments' + fileName,
+        filePath: config.attachmentsDir + fileName
+      };
+    }
+  }
+}
 /**
  * Upload file that is temporarily stored as attachment.
  */
 module.exports = function(change, maindb, config) {
-  if (change.doc && change.doc.localFile && change.doc.localFile === true && change.doc._attachments) {
+  if (change.doc && change.doc.data.localFile && change.doc.data.localFile === true && change.doc._attachments) {
     try {
+
       var currentDoc = change.doc;
-      var fileName = '/patientimages' + currentDoc.fileName;
-      var filePath = config.imagesdir + currentDoc.fileName;
+      var docType = currentDoc._id.substring(0, currentDoc._id.indexOf('_'));
+      var filePaths = getFilePaths(docType, currentDoc.data.fileName, config);
+
       // Make the directory to the file if it doesn't exist
-      mkdirp(path.dirname(filePath), function(err) {
+      mkdirp(path.dirname(filePaths.filePath), function(err) {
         if (err) {
-          console.log('Error mkdirp for ' + path.dirname(filePath), err);
+          console.log('Error mkdirp for ' + path.dirname(filePaths.filePath), err);
           return;
         }
         // Get the file from the couchdb attachment
@@ -23,20 +42,20 @@ module.exports = function(change, maindb, config) {
             return;
           }
           // Write the file to the filesystem
-          fs.writeFile(filePath, body, function(err) {
+          fs.writeFile(filePaths.filePath, body, function(err) {
             if (err) {
-              console.log('Error writing file: ' + fileName, err);
+              console.log('Error writing file: ' + filePaths.filePath, err);
               return;
             }
             // Remove the attachment from the document
-            maindb.attachment.destroy(currentDoc._id, 'file', currentDoc._rev, function(err, body) {
+            maindb.attachment.destroy(currentDoc._id, 'file', {rev: currentDoc._rev }, function(err, body) {
               if (err) {
                 console.log('Error deleting attachment on ' + currentDoc._id + ', rev:' + currentDoc._rev, err);
                 return;
               }
               currentDoc._rev = body.rev;
-              currentDoc.url = fileName;
-              currentDoc.localFile = false;
+              currentDoc.data.url = filePaths;
+              currentDoc.data.localFile = false;
               delete currentDoc._attachments;
               // Update the url
               maindb.insert(currentDoc, currentDoc._id, function(err) {
